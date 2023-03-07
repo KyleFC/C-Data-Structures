3e3c7ea082b54669543f17fbcee4f53636e0e6b4
nathanmaldonado <nathan.maldonado@eagles.cui.edu>
2023-03-07 10:14:31 -0800

    I commented the queue file, same as linked list

--- queuelib.c
+++ queuelib.c
@@ -1,4 +1,18 @@
+/*
+ * @queuelib.c - queue implementation
+ * @brief  This file contains the implementation of the queue library.
+ *
+ * authors: Kyle C, Timmy M, Nathan M
+ * date: March 2022
+ */
+
 #include "queuelib.h"
+
+/*
+ * @brief: This boolean checks if the queue is empty
+ * @param: the queue to be checked
+ * @return: 1 or 0
+ */
 
 int isEmpty(linkedlist **qu)
 {
@@ -12,11 +26,23 @@ int isEmpty(linkedlist **qu)
     return 0;
 }
 
+/*
+ * @brief: This function adds a new node to the end of the queue
+ * @param: the queue to be added to
+ * @return: the new queue
+ */
+
 void enqueue(linkedlist **qu, int n)
 {
     linkedlist *temp = *qu;
     addToEnd(&temp, n);
 }
+
+/*
+ * @brief: This function removes the first node in the queue
+ * @param: the queue to be removed from
+ * @return: the new queue
+ */
 
 int dequeue(linkedlist **qu)
 {
